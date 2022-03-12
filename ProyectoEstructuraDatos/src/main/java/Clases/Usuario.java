fc8b3f082e15900882cc9f5e38e3faa5630431c9
randall21998 <rancabo21998@gmail.com>
2022-03-11 21:53:17 -0600

    se agrega frame y clase usuario con la clase cola

---
+++ ProyectoEstructuraDatos/src/main/java/Clases/Usuario.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author Randall
+ */
+public class Usuario {
+
+    private String Name;
+    private String Apellido;
+    private String Cedula;
+    private String TipodeTransaccion;
+    private String Prioridad;
+
+    public Usuario(String Name, String Apellido, String Cedula, String TipodeTransaccion, String Prioridad) {
+        this.Name = Name;
+        this.Apellido = Apellido;
+        this.Cedula = Cedula;
+        this.TipodeTransaccion = TipodeTransaccion;
+        this.Prioridad = Prioridad;
+    }
+
+    public Usuario() {
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getCedula() {
+        return Cedula;
+    }
+
+    public void setCedula(String Cedula) {
+        this.Cedula = Cedula;
+    }
+
+    public String getTipodeTransaccion() {
+        return TipodeTransaccion;
+    }
+
+    public void setTipodeTransaccion(String TipodeTransaccion) {
+        this.TipodeTransaccion = TipodeTransaccion;
+    }
+
+    public String getPrioridad() {
+        return Prioridad;
+    }
+
+    public void setPrioridad(String Prioridad) {
+        this.Prioridad = Prioridad;
+    }
+    
+}
